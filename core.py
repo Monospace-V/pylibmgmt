02cabd9fb785dd5e448d1ee55714ea73b79bce77
Monospace-V <monospace.substitute@gmail.com>
2024-01-30 11:32:19 +0530

    Create and push core

---
+++ core.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author):
+        self.title=title
+        self.book=title
+        self.name=title
+        self.author=author
+
+class Title(Book):
+    def __init__(self, title, author, genre, id=0):
+        super().__init__(title, author)
+        self.genre=genre
+        self.id=id
+
+class Issue(Book):
+    def __init__(self, title, author, id, date):
+        super().__init__(title, author)
+        self.id=id
+        self.date=date
