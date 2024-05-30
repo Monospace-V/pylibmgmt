84094f45c611c434a18c1c955fec8cf7a09ec68a
Monospace-V <monospace.substitute@gmail.com>
2024-05-30 12:01:40 +0530

    Conventions

--- core.py
+++ core.py
@@ -1,18 +1,18 @@
-class Book:
+class book:
     def __init__(self, title, author):
-        self.title=title
-        self.book=title
-        self.name=title
-        self.author=author
+        self.title = title
+        self.book = title
+        self.name = title
+        self.author = author
 
-class Title(Book):
-    def __init__(self, title, author, genre, id=0):
+class title(book):
+    def __init__(self, title, author, genre, id = 0):
         super().__init__(title, author)
-        self.genre=genre
-        self.id=id
+        self.genre = genre
+        self.id = id
 
-class Issue(Book):
+class issue(book):
     def __init__(self, title, author, id, date):
         super().__init__(title, author)
-        self.id=id
-        self.date=date
+        self.id = id
+        self.date = date
