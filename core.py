edb6a94b90ad9ccfe59101719a0dadf767aa1c2f
Monospace-V <monospace.substitute@gmail.com>
2024-05-30 14:16:10 +0530

    fix convention quick

--- core.py
+++ core.py
@@ -1,17 +1,17 @@
-class book:
+class Book:
     def __init__(self, title, author):
         self.title = title
         self.book = title
         self.name = title
         self.author = author
 
-class title(book):
+class Title(Book):
     def __init__(self, title, author, genre, id = 0):
         super().__init__(title, author)
         self.genre = genre
         self.id = id
 
-class issue(book):
+class Issue(Book):
     def __init__(self, title, author, id, date):
         super().__init__(title, author)
         self.id = id
